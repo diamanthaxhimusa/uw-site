e62332306ed8f7e17c3e01fef1fe267dacaf09b7
diamanthaxhimusa <diamanthaxhimusa@gmail.com>
2018-06-12 15:41:29 +0200

    scroll page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,23 +1,52 @@
 import React, { Component } from 'react';
+import ReactPageScroller from 'react-page-scroller';
 import './Home.css';
 const data = [
+  'Project 1',
+  'Project 2',
+  'Project 3',
+  'Project 4',
+  'Project 5',
+]
+
+const bg = [
+  'http://backgroundcheckall.com/wp-content/uploads/2017/12/work-background-7.jpg',
+  'http://www.bensonhospital.org/wp-content/uploads/2017/08/Desk-Background.jpg',
+  'http://backgroundcheckall.com/wp-content/uploads/2017/12/background-work-10.jpg',
+  'https://i.ytimg.com/vi/o0D6Av1BgUM/maxresdefault.jpg',
+  '#555',
 ]
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentPage: 1 };
+    this._pageScroller = null;
+  }
+
+  goToPage = (eventKey) => {
+    this._pageScroller.goToPage(eventKey);
+  };
+
+  pageOnChange = (number) => {
+    this.setState({ currentPage: number });
+  };
   render() {
     return (
       <div className="container">
+        <ReactPageScroller transitionTimingFunction="cubic-bezier(0.86, 0, 0.07, 1)" ref={c => this._pageScroller = c} pageOnChange={this.pageOnChange}>
         {
           data.map((project, id) => {
             return (
-              <div key={id} className="slide" style={{ backgroundImage: `url(${project.img})` }} >
-                <div className="slide-inside">
-                  <h2>{project.title}</h2>
+              <div className="component" style={{ backgroundImage: `url(${bg[id]})` }}>
+                <div className="title">
+                  <h2>{project}</h2>
                 </div>
               </div>
             );
           })
         }
+        </ReactPageScroller>
       </div>
     );
   }
